293177dda83306394e5c28ca16b1d0f3249e2941
Eran Kampf <eran@ekampf.com>
2023-09-20 13:42:51 -0700

    Revert "go fmt"
    This reverts commit bed899d53807baa0fe905295c983a21b5f886241.

--- golden_test.go
+++ golden_test.go
@@ -1,62 +1,62 @@
 package golden
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
-	"regexp"
-	"strings"
-	"testing"
+    "flag"
+    "fmt"
+    "os"
+    "path/filepath"
+    "regexp"
+    "strings"
+    "testing"
 
-	"github.com/gruntwork-io/terratest/modules/helm"
+    "github.com/gruntwork-io/terratest/modules/helm"
 )
 
 var update = flag.Bool("update", false, "update golden test output files")
 
 func TestHelmRender(t *testing.T) {
-	files, err := os.ReadDir("./test/golden")
-	if err != nil {
-		t.Fatal(err)
-	}
+    files, err := os.ReadDir("./test/golden")
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	for _, f := range files {
-		if !f.IsDir() && strings.HasSuffix(f.Name(), ".yaml") && !strings.HasSuffix(f.Name(), ".golden.yaml") {
-			// Render this values.yaml file
-			output := helm.RenderTemplate(t,
-				&helm.Options{
-					ValuesFiles: []string{"test/golden/" + f.Name()},
-					SetValues: map[string]string{
-						"versionOverrideForTests": "0.0.0-test",
-					},
-				},
-				"./stable/connector",
-				"test",
-				nil,
-			)
+    for _, f := range files {
+        if !f.IsDir() && strings.HasSuffix(f.Name(), ".yaml") && !strings.HasSuffix(f.Name(), ".golden.yaml") {
+            // Render this values.yaml file
+            output := helm.RenderTemplate(t,
+                &helm.Options{
+                    ValuesFiles: []string{"test/golden/" + f.Name()},
+                    SetValues: map[string]string{
+                        "versionOverrideForTests": "0.0.0-test",
+                    },
+                },
+                "./stable/connector",
+                "test",
+                nil,
+            )
 
-			goldenFile := "test/golden/" + strings.TrimSuffix(f.Name(), filepath.Ext(".yaml")) + ".golden.yaml"
-			regex := regexp.MustCompile(`\s+helm.sh/chart:\s+.*`)
-			bytes := regex.ReplaceAll([]byte(output), []byte(""))
+            goldenFile := "test/golden/" + strings.TrimSuffix(f.Name(), filepath.Ext(".yaml")) + ".golden.yaml"
+            regex := regexp.MustCompile(`\s+helm.sh/chart:\s+.*`)
+            bytes := regex.ReplaceAll([]byte(output), []byte(""))
 
-			output = fmt.Sprintf("%s\n", string(bytes))
+            output = fmt.Sprintf("%s\n", string(bytes))
 
-			if *update {
-				err := os.WriteFile(goldenFile, []byte(output), 0644)
-				if err != nil {
-					t.Fatal(err)
-				}
-			}
+            if *update {
+                err := os.WriteFile(goldenFile, []byte(output), 0644)
+                if err != nil {
+                    t.Fatal(err)
+                }
+            }
 
-			expected, err := os.ReadFile(goldenFile)
-			if err != nil {
-				t.Fatal(err)
-			}
+            expected, err := os.ReadFile(goldenFile)
+            if err != nil {
+                t.Fatal(err)
+            }
 
-			if string(expected) != output {
-				t.Fatalf("Expected %s, but got %s\n. Update golden files by running `go test -v ./... -update`", string(expected), output)
-			}
-		}
+            if string(expected) != output {
+                t.Fatalf("Expected %s, but got %s\n. Update golden files by running `go test -v ./... -update`", string(expected), output)
+            }
+        }
 
-	}
+    }
 }
