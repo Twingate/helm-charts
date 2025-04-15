73da9b3b800c7ea04388fc82b284f6614ba93848
Eran Kampf <eran@ekampf.com>
2025-04-15 10:09:21 -0700

    chore try fix publish script

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -6,6 +6,6 @@ git add packages
 git add index.yaml
 git commit -m "publish ${CIRCLE_BUILD_NUM}"
 git checkout gh-pages
-git merge master --strategy-option theirs -v
+git merge master --strategy-option theirs -v --no-edit
 git commit -m "merge commit ${CIRCLE_BUILD_NUM}"
 git push origin gh-pages
