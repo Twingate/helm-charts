192b348be069a859dfc5bd6f71a1cbd9bf097823
Eran Kampf <eran@ekampf.com>
2025-04-15 10:05:56 -0700

    chore: publish script verbosity

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -6,6 +6,6 @@ git add packages
 git add index.yaml
 git commit -m "publish ${CIRCLE_BUILD_NUM}"
 git checkout gh-pages
-git merge master --strategy-option theirs
+git merge master --strategy-option theirs -v
 git commit -m "merge commit ${CIRCLE_BUILD_NUM}"
 git push origin gh-pages
